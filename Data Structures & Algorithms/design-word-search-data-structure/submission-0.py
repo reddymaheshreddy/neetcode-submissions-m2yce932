2a7309eaa7c121bf059fa91d637b2b54c763d940
Reddy Mahesh Reddy Dugganapalli <reddymaheshreddy.d@gmail.com>
2026-04-06 17:36:13 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.word = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(i, node):
+            if i == len(word):
+                return node.word   
+
+            c = word[i]
+
+            if c == ".":
+                for child in node.children.values():
+                    if dfs(i + 1, child):
+                        return True
+                return False
+            else:
+                if c not in node.children:
+                    return False
+                return dfs(i + 1, node.children[c])
+
+        return dfs(0, self.root)
